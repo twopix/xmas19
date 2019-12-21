8238e090e9b816063e1ec448360b48fc3773b4e9
George Melidis <melidis@gmail.com>
2019-12-21 18:50:02 +0200

    change smt

--- src/lib/style.js
+++ src/lib/style.js
@@ -1,7 +1,10 @@
 'use strict'
 
 // Place other JavaScript modules in the 'lib' folder
+
 import 'assets/styles/index.less'
+import 'assets/audio/spring.mp3'
+
 console.log('Style applied')
 
 export default 'style module'
