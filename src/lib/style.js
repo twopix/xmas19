8f588ea807bf3e8a0fa197253e3ea5130b020bac
George Melidis <melidis@gmail.com>
2019-12-23 19:08:47 +0200

    preinstall

--- src/lib/style.js
+++ src/lib/style.js
@@ -3,6 +3,8 @@
 // Place other JavaScript modules in the 'lib' folder
 
 import 'assets/styles/index.less'
+import 'assets/styles/player.less'
+
 import 'assets/audio/spring.mp3'
 
 console.log('Style applied')
